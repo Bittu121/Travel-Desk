88b8994c45c3a2d6d87dc9c82a3b9e2794c50d9e
bittu <bittukumar8713@gmail.com>
2025-08-27 17:56:01 +0530

    create backend server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,26 @@
-console.log("Travel Desk")
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import connectDB from "./db.js";
+import cors from "cors";
+dotenv.config();
+const app = express();
+
+//db connection
+connectDB();
+
+//middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
