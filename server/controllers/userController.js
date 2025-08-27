40e2774dd98785cd2d1314d00bd18b360f671d39
bittu <bittukumar8713@gmail.com>
2025-08-27 18:09:53 +0530

    setup controller and routing for login logout and get user details

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,2 +1,11 @@
 //register user
 export const register = async (req, res) => {};
+
+//login user
+export const login = async (req, res) => {};
+
+//logout user
+export const logout = async (req, res) => {};
+
+//get user data
+export const getUserData = async (req, res) => {};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,14 @@
 import express from "express";
-import { register } from "../controllers/userController.js";
+import {
+  login,
+  logout,
+  register,
+  getUserData,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/logout").post(logout);
+router.route("/").get(getUserData);
 export default router;
